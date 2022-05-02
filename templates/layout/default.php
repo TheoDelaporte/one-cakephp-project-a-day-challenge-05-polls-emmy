0d8d5d8a942c46c51d2b1c42bd5f6d194ebc91a9
Théo Delaporte <theodelaporte@MacBook-de-Theo.local>
2022-05-02 11:55:43 +0200

    bulma framework install

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -13,11 +14,11 @@
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  * @var \App\View\AppView $this
  */
-
 $cakeDescription = 'CakePHP: the rapid development php framework';
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -26,32 +27,24 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
-
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
-
     <?= $this->fetch('meta') ?>
+    <?= $this->Html->css(['bulma.min.css']) ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
+
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
+    <section class="section">
         <div class="container">
-            <?= $this->Flash->render() ?>
+            <h1 class="title">
+                <?= $this->fetch('title') ?>
+            </h1>
             <?= $this->fetch('content') ?>
+            <?= $this->Flash->render() ?>
         </div>
-    </main>
+    </section>
     <footer>
     </footer>
 </body>
+
 </html>
